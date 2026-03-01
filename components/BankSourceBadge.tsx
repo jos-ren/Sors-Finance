c672b19592eb3943c6950011effa4a2cc135a623
Josh Renema <60946895+jos-ren@users.noreply.github.com>
2026-02-28 23:18:09 -0800

    add bank mapper, update version. 1.0 release

--- components/BankSourceBadge.tsx
+++ components/BankSourceBadge.tsx
@@ -11,8 +11,54 @@ import {
 } from "@/components/ui/tooltip";
 
 // Map of bank sources to their logo paths
+// Keys match Plaid institutionName values and/or CSV template names
 const BANK_LOGOS: Record<string, string> = {
-  // Add custom bank logos here
+  // TD
+  "TD": "/logos/banks/td.png",
+  "TD Bank": "/logos/banks/td.png",
+  "TD Canada Trust": "/logos/banks/td.png",
+
+  // RBC
+  "RBC": "/logos/banks/rbc.png",
+  "Royal Bank of Canada": "/logos/banks/rbc.png",
+  "RBC Royal Bank": "/logos/banks/rbc.png",
+
+  // CIBC
+  "CIBC": "/logos/banks/cibc.png",
+
+  // BMO
+  "BMO": "/logos/banks/bmo.png",
+  "Bank of Montreal": "/logos/banks/bmo.png",
+  "BMO Bank of Montreal": "/logos/banks/bmo.png",
+
+  // Scotiabank
+  "Scotiabank": "/logos/banks/scotiabank.svg",
+  "Bank of Nova Scotia": "/logos/banks/scotiabank.svg",
+
+  // National Bank
+  "National Bank": "/logos/banks/nationalbank.png",
+  "National Bank of Canada": "/logos/banks/nationalbank.png",
+
+  // Wealthsimple
+  "Wealthsimple": "/logos/banks/wealthsimple.png",
+
+  // Tangerine
+  "Tangerine": "/logos/banks/tangerine.png",
+
+  // American Express
+  "Amex": "/logos/banks/amex.png",
+  "American Express": "/logos/banks/amex.png",
+
+  // ATB Financial
+  "ATB": "/logos/banks/atb.png",
+  "ATB Financial": "/logos/banks/atb.png",
+
+  // Desjardins
+  "Desjardins": "/logos/banks/desjardins.jpg",
+
+  // Vancity
+  "Vancity": "/logos/banks/vancity.jpg",
+  "Vancouver City Savings": "/logos/banks/vancity.jpg",
 };
 
 interface BankSourceBadgeProps {
